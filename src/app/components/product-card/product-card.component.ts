50e7d16ac0121a1ab7043b1043b63931c934fd60
MatiasBelsterli <matias.belsterli@gmail.com>
2024-05-30 21:50:13 -0300

    add shopping cart service

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/products/product.model';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -7,9 +8,22 @@ import { Product } from '../../models/products/product.model';
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
+  constructor(private cartService: CartService) {}
+  
   @Input() product?: Product;
+  quantity: number = 0;
 
-  addToCart(product: Product) {
-    console.log('Product added to cart', this.product);
+  addToCart(product: Product, quantity: number) {
+    this.cartService.add(product, quantity);
+  }
+
+  increaseQuantity() {
+    this.quantity++;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 0) {
+      this.quantity--;
+    }
   }
 }
