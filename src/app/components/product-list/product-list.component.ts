aa240b7f1edd47c463173ef295ec6d6fb89dbb4b
Tiago Silva <tiaval@hotmail.es>
2024-06-18 13:00:27 -0300

    feature/30-productPageFilters

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/products/product.service';
-import { Observable, catchError } from 'rxjs';
-import { Product } from '../../models/products/product.model';
+import { Observable, catchError, of } from 'rxjs';
 import { AuthService } from "../../services/auth/auth-service/auth.service";
 import { UserRole } from "../../enums/user-role";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-product-list',
@@ -11,9 +11,15 @@ import { UserRole } from "../../enums/user-role";
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent implements OnInit {
-  public productList$: Observable<Product[]>;
+  productList$: Observable<any> = of({ products: [], totalPages: 0 });
   public hasError: boolean = false;
   userType: String = '';
+
+  searchTerm = new FormControl('');
+
+  totalPages: number = 0;
+  currentPage = 1;
+  limit = 8;
 
   ngOnInit() {
     this.authService.isUserType.subscribe(userType => {
@@ -23,7 +29,7 @@ export class ProductListComponent implements OnInit {
 
   deleteProduct(id: number) {
     this.productService.deleteProduct(id).subscribe({
-      next: value => {
+      next: () => {
         document.querySelector(`#product_${id}`)?.remove()
       }, error: err => {
         console.error(err)
@@ -31,13 +37,26 @@ export class ProductListComponent implements OnInit {
     })
   }
 
+  loadProducts() {
+    this.productList$ = this.productService.getProducts(this.currentPage, this.limit, this.searchTerm.value ?? '').pipe(
+      catchError(err => {
+        console.error('Error getting products', err);
+        this.hasError = true;
+        return of({ products: [], totalPages: 0 });
+      })
+    );
+    this.productList$.subscribe(data => {
+      this.totalPages = data.totalPages;
+    });
+  }
   constructor(private productService: ProductService, private authService: AuthService) {
-    this.productList$ = this.productService.getProducts().pipe(catchError(err => {
-      console.error('Error getting products', err);
-      this.hasError = true;
-      // throw new Error(err);
-      return [];
-    }));
+    this.loadProducts();
+  }
+
+  onPageChange(page: number) {
+    if (page === this.currentPage) return
+    this.currentPage = page;
+    this.loadProducts();
   }
 
   protected readonly UserRole = UserRole;
