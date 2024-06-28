f2327eabdd9dcc74976bf3d7a67362908c111299
MatiasBelsterli <matias.belsterli@gmail.com>
2024-06-28 14:35:02 -0300

    add supplies list & crud

--- src/app/components/supplies/supply-modal/supply-modal.component.ts
+++ src/app/components/supplies/supply-modal/supply-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Supply } from '../../../models/supplies/supply.model';
+import { Supply, Unit } from '../../../models/supplies/supply.model';
 
 @Component({
   selector: 'app-supply-modal',
@@ -17,11 +17,13 @@ export class SupplyModalComponent implements OnChanges {
 
   supplyForm: FormGroup;
   formSubmitted = false;
+  units = Object.values(Unit);
 
   constructor(private fb: FormBuilder) {
     this.supplyForm = this.fb.group({
       name: ['', Validators.required],
-      price: ['', [Validators.required, Validators.min(0.001)]],
+      price: ['', [Validators.required, Validators.min(0.0001)]],
+      unit: [null, Validators.required],
     });
   }
 
