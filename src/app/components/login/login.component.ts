f1226718822e0f240e975b04b0e1b52a24586ab3
MatiasBelsterli <matias.belsterli@gmail.com>
2024-06-30 22:05:26 -0300

    remove mocked users

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,8 +15,8 @@ export class LoginComponent {
 
   constructor(private authService: AuthService, private router: Router, fb: FormBuilder) {
     this.loginForm = fb.group({
-      email: ['admin@example.com', Validators.required],
-      password: ['admin123', Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
@@ -31,28 +31,5 @@ export class LoginComponent {
         this.errorMessage = 'Invalid email or password';
       }
     );
-  }
-
-
-  // TODO delete
-  log(credentials: any): void {
-    this.authService.login(credentials).subscribe(
-      response => {
-        localStorage.setItem('token', response.token);
-        this.router.navigate(['/']);
-      },
-      error => {
-        this.errorMessage = 'Invalid email or password';
-      }
-    );
-  }
-  logBaker() {
-    this.log({email: 'baker@example.com', password: 'baker123'})
-  }
-  logUser1() {
-    this.log({email: 'user@example.com', password: 'user123'})
-  }
-  logUser2() {
-    this.log({email: 'user2@example.com', password: 'user123'})
   }
 }
